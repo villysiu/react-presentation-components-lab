25586ae803b24332cab9c04b9309c13424a6885d
villysiu <villysiu@gmail.com>
2021-07-23 16:28:28 -0700

    commit tree ae1d64bf16a1a44cf74dbe220e351576cd492bcb

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,29 @@
 // Code SimpleComponent Here
+import React, { Component } from 'react'
+
+class SimpleComponent extends Component {
+    constructor(props) {
+      super(props);
+  
+      // Initial state here...
+      this.state = {
+          mood: 'happy'
+      };
+    }
+  
+    handleClick = () => {
+      // Probably do some work to update the state
+     
+        this.setState({
+           mood: (this.state.mood==='happy') ? 'sad' : 'happy'
+        })
+    }
+  
+    render() {
+      // Return JSX that renders into HTML
+      return (
+          <div onClick={this.handleClick}>{this.state.mood}</div>
+      )
+    }
+  } 
+  export default SimpleComponent;
